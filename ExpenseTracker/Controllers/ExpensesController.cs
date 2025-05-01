981e46891f448e82728db5143ea278faa10e7876
MD TAHER BIN OMAR HIJBULLAH <hijbullah119445@gmail.com>
2025-05-01 11:44:25 +0600

    Initial commit of ExpenseTracker project

---
+++ ExpenseTracker/Controllers/ExpensesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace ExpenseTracker.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly ExpenseDbContext _context;
+
+        public ExpensesController(ExpenseDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Expenses
+        public IActionResult Index()
+        {
+            var expenses = _context.Expenses.ToList();
+            return View(expenses);
+        }
+
+        // GET: Expenses/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Expenses/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(expense);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(expense);
+        }
+        public IActionResult Summary()
+        {
+            var summary = _context.Expenses
+                .GroupBy(e => e.Category)
+                .Select(g => new { Category = g.Key, Total = g.Sum(e => e.Amount) })
+                .ToList();
+            return View(summary);
+        }
+    }
+}
---
+++ ExpenseTracker/Models/Expense.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; } // Primary key
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Category { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0")]
+        public decimal Amount { get; set; }
+
+        [StringLength(200)]
+        public string Description { get; set; }
+    }
+}
